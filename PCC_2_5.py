fd2d14aa54623f354d428b35e9093f49146aaeb9
Ruwzy <52026822+Ruwzy@users.noreply.github.com>
2019-06-20 14:57:45 +0800

    Delete PCC_2_5.py

--- PCC_2_5.py
+++
@@ -1,3 +0,0 @@
-# Ruby,05222019,打印Gandhi的名言
-print('Gandhi once said, "You must be the change you want to see in the world."')
-print("Gandhi once said, \"You must be the change you want to see in the world.\"")
