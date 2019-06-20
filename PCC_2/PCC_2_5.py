bd50b7079f53e7c80b1990e9bb8e3e1938797f73
Ruwzy <52026822+Ruwzy@users.noreply.github.com>
2019-06-20 14:57:03 +0800

    Add files via upload

---
+++ PCC_2/PCC_2_5.py
@@ -0,0 +1,3 @@
+# Ruby,05222019,打印Gandhi的名言
+print('Gandhi once said, "You must be the change you want to see in the world."')
+print("Gandhi once said, \"You must be the change you want to see in the world.\"")
