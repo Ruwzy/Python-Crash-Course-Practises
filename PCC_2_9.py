fb5694e7c6f9c593e740498483bf5902c4fe8258
Ruwzy <52026822+Ruwzy@users.noreply.github.com>
2019-06-20 14:58:20 +0800

    Delete PCC_2_9.py

--- PCC_2_9.py
+++
@@ -1,7 +0,0 @@
-fav_num = "8"
-print("My favourite number is " + fav_num)
-
-#此处有疑问，关于= 和==， ==应该是让变量和某个int相同，但是出来还是str，疑惑
-fav_num == 8
-print(type(fav_num))
-print("My favourite number is " + fav_num)
