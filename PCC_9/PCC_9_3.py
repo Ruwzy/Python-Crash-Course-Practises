94e467f7cb27d78a58f183cf3f12c53413975b6b
Rubywzy <52026822+Ruwzy@users.noreply.github.com>
2019-06-21 09:26:59 +0800

    Update PCC_9_3.py

--- PCC_9/PCC_9_3.py
+++ PCC_9/PCC_9_3.py
@@ -1,7 +1,7 @@
 class User():
     '''build a class named user'''
 
-    def _init_(self, first_name, last_name, age, gender):
+    def __init__(self, first_name, last_name, age, gender):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
