c495b255f294c80fc732f7803ee5a5e19f970633
Rubywzy <52026822+Ruwzy@users.noreply.github.com>
2019-08-22 09:07:03 +0800

    Add files via upload

---
+++ PCC_12/practice_12_3/game_functions.py
@@ -0,0 +1,48 @@
+import sys
+
+import pygame
+
+def check_keydown_events(event, ship):
+    """response the keydown event"""
+    if event.key ==pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
+
+def check_keyup_events(event, ship):
+    """response to the keyup event"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+def check_events(ship):
+    """response for the keyboard and mouse event"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screen(ai_settings, screen, ship):
+    """update the image on the screen, then change to the new screen."""
+    #repaint the screen every time the loop starts
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    #let the last screen can be seen
+    pygame.display.flip()
+
+
---
+++ PCC_12/practice_12_3/ship.py
@@ -0,0 +1,51 @@
+import pygame
+
+
+class Ship():
+
+    def __init__(self, ai_settings, screen):
+        """ initialize the ship's setting and its original place"""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # load the ship's image and the outside rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = self.screen.get_rect()
+
+        # set every new ship at the screen's center bottom
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
+        # make the ship's setting can sotre float
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
+    
+
+        # moving signal
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """adjust the ship's position based on the moving signal"""
+        # upade the center value not the rect value
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.centerx -= self.ai_settings.ship_spped_factor
+        if self.moving_up and self.rect.top > 0:
+            self.centery -= self.ai_settings.ship_spped_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
+
+
+
+    def blitme(self):
+        """draw a ship at a pointed place"""
+        self.screen.blit(self.image, self.rect)
+        
